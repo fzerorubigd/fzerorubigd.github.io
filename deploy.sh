ff1f278aeb1a96b0892f68be9d3e162a3aa6475b
fzerorubigd <fzerorubigd@gmail.com>
2014-11-08 20:36:43 +0330

    rebuilding site Sat Nov  8 20:36:43 IRST 2014

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project. 
-hugo
+hugo -t hugo-rtl
 
 # Add changes to git.
 git add -A
@@ -13,7 +13,7 @@ msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
-git commit -m "$msg"
+git commit -aSs -m "$msg"
 
 # Push source and build repos.
 git push origin master
